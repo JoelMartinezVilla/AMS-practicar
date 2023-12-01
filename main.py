a135cd43496a09889989127c978b3b68472c6b7b
JoelMartinezVilla <jmartinezvilla.cf@iesesteveterradas.cat>
2023-12-01 16:27:34 +0100

    añadido main

---
+++ main.py
@@ -0,0 +1,8 @@
+from holaMundo import hola_mundo
+from queCojonesEstoyHaciendo import me_cago_en_dios
+
+hola_mundo()
+
+nivel_cagada = input("En que nivel de cagada estás hoy? ")
+
+me_cago_en_dios(nivel_cagada)
