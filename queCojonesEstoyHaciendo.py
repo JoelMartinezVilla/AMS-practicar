2f26f27ec0d4b26d90633fc4b9b02f9c9598c349
JoelMartinezVilla <jmartinezvilla.cf@iesesteveterradas.cat>
2023-12-01 16:22:17 +0100

    añadido queCojonesEstoyHaciendo y holaMundo

---
+++ queCojonesEstoyHaciendo.py
@@ -0,0 +1,6 @@
+def me_cago_en_dios(nivel_cagada):
+    if not nivel_cagada.isdigit():
+        print("Encima eres tonto")
+    else:
+        for i in range(nivel_cagada):
+            print("Me cago en todo")
