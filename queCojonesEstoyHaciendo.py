ae2508ae224acbab40e5b1138dca87f068dd09dd
JoelMartinezVilla <jmartinezvilla.cf@iesesteveterradas.cat>
2023-12-01 16:28:18 +0100

    actualizado queCojonesEstoyHaciendo

--- queCojonesEstoyHaciendo.py
+++ queCojonesEstoyHaciendo.py
@@ -2,5 +2,6 @@ def me_cago_en_dios(nivel_cagada):
     if not nivel_cagada.isdigit():
         print("Encima eres tonto")
     else:
-        for i in range(nivel_cagada):
+        nivel_cagada_int = int(nivel_cagada)
+        for i in range(nivel_cagada_int):
             print("Me cago en todo")
